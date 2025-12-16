c4c28f65509603ff5abc15cb0faa55baa08c4099
Mathew Goldsborough <mat@onlyonet.com>
2025-12-15 17:15:04 -1000

    This is a combination of 4 commits. fixed image
    checkpoint
    
    checkpoint
    
    moving images to assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.goldsborough.io',
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()],
 });
